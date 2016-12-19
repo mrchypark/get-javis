743923bdb964d409e9cd039f12993be23e379823
mrchypark <mrchypark@gmail.com>
2016-12-19 17:13:08 +0900

    go

--- sel.R
+++ sel.R
@@ -30,10 +30,17 @@ tem <- remDr$getPageSource()[[1]]
 tem <- tem %>% read_html() %>% html_node("pre") %>% html_text()
 fromJSON(tem)
 
-slackrSetup(channel="#teamroom", 
+slackrSetup(channel="#test", 
             incoming_webhook_url=incoming_webhook_url,
             api_token=api_token)
 
-text_slackr(iconv("다",to="UTF-8"))
+slackr_msg(iconv("테스트 중입니다.",to="UTF-8"))
 
 
+
+library(MASS)
+str(Cars93)
+with(Cars93, cov(x=MPG.highway, 
+                                   y=Weight, 
+                                   use="complete.obs", 
+                                   method=c("pearson")))
