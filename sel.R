10bcc0bfa402007dd58a4034abb8e81d704e23d6
Chan-Yub Park <mrchypark@gmail.com>
2017-01-03 18:25:13 +0900

    Update sel.R

--- sel.R
+++ sel.R
@@ -33,28 +33,6 @@ slackrSetup(channel="#test",
             incoming_webhook_url=incoming_webhook_url,
             api_token=api_token)
 
-if(chk[1]!=0){
-            slackr_msg(iconv("개인 영수증이 확인되었습니다. 수정해 주세요.",to="UTF-8"))
+if(chk[1]!="0"){
+            text_slackr(iconv("개인 영수증이 확인되었습니다. 수정해 주세요.",to="UTF-8"), as_user=FALSE)
             }
-
-
-remDr$navigate("https://manager.jobis.co/company#tab2")
-
-tem <- remDr$getPageSource()[[1]]
-tem <- tem %>% read_html() %>% html_node("pre") %>% html_text()
-fromJSON(tem)
-
-slackrSetup(channel="#test", 
-            incoming_webhook_url=incoming_webhook_url,
-            api_token=api_token)
-
-slackr_msg(iconv("테스트 중입니다.",to="UTF-8"))
-
-
-
-library(MASS)
-str(Cars93)
-with(Cars93, cov(x=MPG.highway, 
-                                   y=Weight, 
-                                   use="complete.obs", 
-                                   method=c("pearson")))
